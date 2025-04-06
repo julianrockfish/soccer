901be3339aa431cacc255bfcb1ec99362fa2740e
Julian <4061185+julianrockfish@users.noreply.github.com>
2025-04-06 17:02:54 -0500

    decorator
    for admin

--- soccer-tracker/games/views.py
+++ soccer-tracker/games/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Game
 from .forms import GameForm  # You'll create this form in the next step
+from django.contrib.admin.views.decorators import staff_member_required  # Import this
 
 # Create Game
+@staff_member_required
 def create_game(request):
     if request.method == 'POST':
         form = GameForm(request.POST, request.FILES)  # Include FILES for media
@@ -17,6 +19,7 @@ def create_game(request):
     return render(request, 'games/create_game.html', {'form': form})
 
 # Update Game
+@staff_member_required
 def update_game(request, pk):
     game = get_object_or_404(Game, pk=pk)
     if request.method == 'POST':
@@ -29,6 +32,7 @@ def update_game(request, pk):
     return render(request, 'games/update_game.html', {'form': form})
 
 # Delete Game
+@staff_member_required
 def delete_game(request, pk):
     game = get_object_or_404(Game, pk=pk)
     if request.method == 'POST':
@@ -40,3 +44,9 @@ def delete_game(request, pk):
 def list_games(request):
     games = Game.objects.all()  # Get all games
     return render(request, 'games/list_games.html', {'games': games})
+
+
+# Game Detail View
+def game_detail(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    return render(request, 'games/game_detail.html', {'game': game})
